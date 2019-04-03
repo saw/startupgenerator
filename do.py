bbf854e4e17bd549b02454530e48ff1c9d482b51
Stephen <stephen.woods@salesforce.com>
2019-04-02 20:33:28 -0700

    herewego;

---
+++ do.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import numpy as np
+import os
+import time
+
+text = open('out.txt', 'rb').read().decode(encoding='utf-8')
+print ('Length of text: {} characters'.format(len(text)))
+vocab = sorted(set(text))
+# print ('{} unique characters'.format(len(vocab)))
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+# print('{')
+# for char,_ in zip(char2idx, range(20)):
+#     print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+# print('  ...\n}')
+
+# # Show how the first 13 characters from the text are mapped to integers
+# print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 50
+examples_per_epoch = len(text)//seq_length
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+# for i in char_dataset.take(5):
+#   print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+# for item in sequences.take(5):
+#   print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# for input_example, target_example in  dataset.take(1):
+#   print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+#   print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+# Batch size 
+BATCH_SIZE = 64
+steps_per_epoch = examples_per_epoch//BATCH_SIZE
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences, 
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead, 
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+print('dataset {}', dataset)
+
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+# The embedding dimension 
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+
+if tf.test.is_gpu_available():
+  print("we have gpu")
+  rnn = tf.keras.layers.CuDNNGRU
+else:
+  import functools
+  rnn = functools.partial(
+    tf.keras.layers.GRU, recurrent_activation='sigmoid')
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, 
+                              batch_input_shape=[batch_size, None]),
+    rnn(rnn_units,
+        return_sequences=True, 
+        recurrent_initializer='glorot_uniform',
+        stateful=True),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(
+  vocab_size = len(vocab), 
+  embedding_dim=embedding_dim, 
+  rnn_units=rnn_units, 
+  batch_size=BATCH_SIZE)
+
+for input_example_batch, target_example_batch in dataset.take(1): 
+  example_batch_predictions = model(input_example_batch)
+  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
+model.summary()
+
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
+
+print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
+print()
+print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices ])))
+
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+example_batch_loss  = loss(target_example_batch, example_batch_predictions)
+print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)") 
+print("scalar_loss:      ", example_batch_loss.numpy().mean())
+
+model.compile(
+    optimizer = tf.train.AdamOptimizer(),
+    loss = loss)
+
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+EPOCHS=3
+history = model.fit(dataset.repeat(), epochs=EPOCHS, steps_per_epoch=steps_per_epoch, callbacks=[checkpoint_callback])
